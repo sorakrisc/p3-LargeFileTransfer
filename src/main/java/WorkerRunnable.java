6b5958ee25fdadf47aee72935ff02519fd448440
James <csorakris@gmail.com>
2016-12-13 22:12:36 +0700

    code clean up: delete some useless comments

--- src/main/java/WorkerRunnable.java
+++ src/main/java/WorkerRunnable.java
@@ -16,26 +16,14 @@ public class WorkerRunnable implements Runnable{
     public void run() {
         try {
             InputStream input  = clientSocket.getInputStream();
-            OutputStream output = clientSocket.getOutputStream();
-//            long time = System.currentTimeMillis();
-//            output.write(("HTTP/1.1 200 OK\n\nWorkerRunnable: " +
-//                    this.serverText + " - " +
-//                    time +
-//                    "").getBytes());
-
-
             BufferedReader inFromClient =
                     new BufferedReader(new InputStreamReader(input));
-//            DataOutputStream outToClient = new DataOutputStream(output);
             String clientSentence = inFromClient.readLine();
             String senderIp=clientSocket.getRemoteSocketAddress().toString();
             System.out.println(portRemover(senderIp)+": "+clientSentence);
-//            runDiscovery.prgtrkMap.put(portRemover(senderIp),clientSentence);
-//            output.close();
             input.close();
 
         } catch (IOException e) {
-            //report exception somewhere.
             e.printStackTrace();
         }
     }
